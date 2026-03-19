f2aa20a72914eb7b8af90d16fdfecc992e5d7e2a
mizuki-y <mizuki-y@syati.info>
2026-03-20 02:37:30 +0900

    Refactor admin migration to create table and add Devise modules for authentication

--- db/migrate/20260319165213_add_devise_to_admins.rb
+++ db/migrate/20260319165213_add_devise_to_admins.rb
@@ -2,10 +2,10 @@
 
 class AddDeviseToAdmins < ActiveRecord::Migration[8.1]
   def self.up
-    change_table :admins do |t|
+    create_table :admins do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,              null: false
+      t.string :encrypted_password, null: false
 
       ## Recoverable
       t.string   :reset_password_token
@@ -22,10 +22,10 @@ class AddDeviseToAdmins < ActiveRecord::Migration[8.1]
       t.string   :last_sign_in_ip
 
       ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
       t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
@@ -39,8 +39,8 @@ class AddDeviseToAdmins < ActiveRecord::Migration[8.1]
 
     add_index :admins, :email,                unique: true
     add_index :admins, :reset_password_token, unique: true
-    # add_index :admins, :confirmation_token,   unique: true
-    # add_index :admins, :unlock_token,         unique: true
+    add_index :admins, :confirmation_token,   unique: true
+    add_index :admins, :unlock_token,         unique: true
   end
 
   def self.down
