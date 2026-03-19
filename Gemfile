ccbc14e5a9a10730d3a0920b53180eaf68a2f1bb
mizuki-y <mizuki-y@syati.info>
2026-03-20 02:45:00 +0900

    Add omniauth and omniauth-rails_csrf_protection gems for authentication

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem "stimulus-rails"
 
 # Application assets
 gem "devise"
+gem "omniauth"
+gem 'omniauth-rails_csrf_protection'
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -1,3 +1,33 @@
+# == Schema Information
+#
+# Table name: admins
+#
+#  id                     :bigint           not null, primary key
+#  confirmation_sent_at   :datetime
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  current_sign_in_at     :datetime
+#  current_sign_in_ip     :string
+#  email                  :string           not null
+#  encrypted_password     :string           not null
+#  failed_attempts        :integer          default(0), not null
+#  last_sign_in_at        :datetime
+#  last_sign_in_ip        :string
+#  locked_at              :datetime
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  sign_in_count          :integer          default(0), not null
+#  unconfirmed_email      :string
+#  unlock_token           :string
+#
+# Indexes
+#
+#  index_admins_on_confirmation_token    (confirmation_token) UNIQUE
+#  index_admins_on_email                 (email) UNIQUE
+#  index_admins_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_admins_on_unlock_token          (unlock_token) UNIQUE
+#
 FactoryBot.define do
   factory :admin do
     name { "MyString" }
