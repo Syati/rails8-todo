d9f7efd6a5eee96e23ead40d93786bbad59433cb
mizuki-y <mizuki-y@syati.info>
2026-03-22 04:37:05 +0900

    Add locale switching functionality and set default locale to Japanese

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
   before_action :authenticate_admin!, unless: :devise_controller?
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+
+  def switch_locale(&action)
+    locale = params[:locale] || session[:locale] || I18n.default_locale
+    session[:locale] = locale
+    I18n.with_locale(locale, &action)
+  end
 end
