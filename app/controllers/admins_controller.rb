18fb8240ba7259571b28e7bce5e4619fffaafd4e
mizuki-y <mizuki-y@syati.info>
2026-04-04 04:59:38 +0900

    feat(admins): implement scenario 1 admin index

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,9 @@
+class AdminsController < ApplicationController
+  PER_PAGE = 30
+
+  def index
+    @page = [params.fetch(:page, 1).to_i, 1].max
+    @admins = Admin.order(id: :desc).offset((@page - 1) * PER_PAGE).limit(PER_PAGE)
+    @has_next_page = Admin.count > (@page * PER_PAGE)
+  end
+end
